194d6ee8a65521e5328941e41bb03af4e4e2197e
Almir Santos <almir.santos@umanni.com.br>
2018-10-19 18:58:59 -0300

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
+    <ul>
+      <li v-for="contributor in contributors" :key="contributor.id">
+        <span v-for="week in contributor.weeks" :key="week.id">
+          {{week.a}} | {{week.d}}
+        </span>
+      </li>
+    </ul>
+    <ul>
+      <li v-for="repository in repositories" :key="repository.id">
+        {{repository.name}}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import Axios from 'axios';
+export default {
+  data(){
+    return {
+      repositories: [],
+      contributors: []
+    }
+  },
+  mounted(){
+    Axios.get('https://api.github.com/users/name/repos').then((resp)=>{
+      this.repositories = resp.data
+      Axios.get(`https://api.github.com/repos/${resp.data[0].owner.login}/${resp.data[0].name}/stats/contributors`).then((contributors => {
+        console.log(contributors)
+        this.contributors = contributors.data
+      }))
+      
+      
+    })
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -27,3 +62,4 @@
   }
 }
 </style>
+
